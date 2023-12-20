1523504827b3baaf80390d32ebdce8a4156e2851
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-12-20 16:54:10 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -20,20 +20,12 @@ app.use(
   })
 );
 
-<<<<<<< HEAD
 app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({ limit:'50mb',extended:true }))
 app.use(cookieParser())
 
-app.use(cors({
-  origin:true,
-  credentials: true,
-}
-));
-
+app.use(cors());
  
-=======
->>>>>>> 8919f8c34043c13de1b58dbecbdf316d9da2519b
 app.get("/", (req, res) => {
   res.send(
     `<h1>Site is workin fine.Click <a href=${process.env.FRONTEND_URL}>Here </a> to get frontend</h1>`
