c3e0cfd74345adcf505b01b77c99d54ac51c8deb
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-12-20 15:00:03 +0530

    cls

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,33 @@
-import express from "express"
-import post from "./routes/post.js"
-import user from "./routes/user.js"
+import express from "express";
+import post from "./routes/post.js";
+import user from "./routes/user.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {config} from "dotenv"
+import { config } from "dotenv";
 
 config();
-const app=express();
+const app = express();
 //middlewares
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit:'50mb'}))
-app.use(express.urlencoded({ limit:'50mb',extended:true }))
-app.use(cookieParser())
-
-app.use(cors({
-  // origin: process.env.FRONTEND_URL,
-  credentials: true,
-}
-));
-
- 
 app.get("/", (req, res) => {
-  res.send(`<h1>Site is workin fine.Click <a href=${process.env.FRONTEND_URL}>Here </a> to get frontend</h1>`)
-})
+  res.send(
+    `<h1>Site is workin fine.Click <a href=${process.env.FRONTEND_URL}>Here </a> to get frontend</h1>`
+  );
+});
 
+app.use("/api/v1", post);
 
-
-app.use("/api/v1",post)
-
-
-app.use("/api/v1",user)
-
-
-
-
+app.use("/api/v1", user);
 
 export default app;
