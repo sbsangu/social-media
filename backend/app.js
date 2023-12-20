114b749a8f24adb23c82aae68b1c38ec71e45277
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-12-20 13:58:43 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ limit:'50mb',extended:true }))
 app.use(cookieParser())
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  // origin: process.env.FRONTEND_URL,
   credentials: true,
 }
 ));
